dd3b9294ee72491d42779011cfa8daf549c2fb52
My Name <theilyashulepov@gmail.com>
2023-07-26 19:30:17 +0300

    feat: validation logic

--- index.js
+++ index.js
@@ -2,17 +2,87 @@ const submit = document.querySelector(".button--submit");
 const inputDay = document.querySelector("#day");
 const inputMonth = document.querySelector("#month");
 const inputYear = document.querySelector("#year");
-const today = new Date();
+
 const outputDay = document.querySelector("#output-day");
 const outputMonth = document.querySelector("#output-month");
 const outputYear = document.querySelector("#output-year");
 
-submit.addEventListener("click", countAge);
+const inputs = [inputDay, inputMonth, inputYear];
+const NUMBER_OF_FIELDS = 3;
+const today = new Date();
+
+submit.addEventListener("click", validateInput);
+
+function validateInput() {
+    let flagCount = 0;
+    inputs.forEach((input) => {
+        let notif = document.querySelector(`.notification--${input.id}`);
+
+        if (checkForEmpty(input, notif) && checkForRange(input, notif))
+            flagCount += 1;
+    });
+    if (inputDay.value <= 31 && flagCount === NUMBER_OF_FIELDS) {
+        flagCount += checkDateForValidity(+inputMonth.value, +inputDay.value);
+    }
+    if (flagCount === NUMBER_OF_FIELDS + 1) countAge();
+}
+
+function checkDateForValidity(month, day) {
+    let isValid = true;
+    let maxDays = 31;
+    if (month === 2) maxDays = 28;
+    else if ([4, 6, 9, 11].includes(month)) {
+        maxDays = 30;
+    }
+    if (day > maxDays) {
+        document.querySelector(`.notification--day`).textContent =
+            "Must be a valid date";
+        isValid = false;
+    }
+    return isValid;
+}
+
+function checkForEmpty(input, notif) {
+    let isValid = true;
+    if (!input.value) {
+        notif.textContent = "The field is required";
+        isValid = false;
+    } else {
+        notif.textContent = "";
+    }
+    return isValid;
+}
+
+function checkForRange(input, notif) {
+    let isValid = true;
+    if (
+        input.id === "day" &&
+        (isNaN(input.value) || input.value < 1 || input.value > 31)
+    ) {
+        notif.textContent = "Must be a valid day";
+        isValid = false;
+    } else if (
+        input.id === "month" &&
+        (isNaN(input.value) || input.value < 1 || input.value > 12)
+    ) {
+        notif.textContent = "Must be a valid month";
+        isValid = false;
+    } else if (input.id === "year") {
+        if (isNaN(input.value) || input.value < 1900) {
+            notif.textContent = "Must be a valid year";
+            isValid = false;
+        } else if (input.value > today.getFullYear()) {
+            notif.textContent = "Must be in the past";
+            isValid = false;
+        }
+    }
+    return isValid;
+}
 
 function countAge() {
-    const birthDay = inputDay.value || 8;
-    const birthMonth = +inputMonth.value || 4;
-    const birthYear = inputYear.value || 2002;
+    const birthDay = inputDay.value;
+    const birthMonth = +inputMonth.value;
+    const birthYear = inputYear.value;
 
     const currDay = today.getDate();
     const currMonth = today.getMonth() + 1;
